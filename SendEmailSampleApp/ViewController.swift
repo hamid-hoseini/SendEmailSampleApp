0c0e4bd70d9d795f05e709838c553468de4a4209
Hamid <mail.hamid.hoseini@gmail.com>
2020-11-09 22:50:13 -0800

    Initial Commit

---
+++ SendEmailSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendEmailSampleApp
+//
+//  Created by Hamid Hoseini on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
